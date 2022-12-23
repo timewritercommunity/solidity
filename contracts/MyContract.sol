98d1b927fb56c77f44d946b9c566331394977f87
nullchimp <timo.von.gruenewaldt@gmail.com>
2022-12-23 23:30:24 +0100

    Fixed some errors

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,15 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 contract MyContract {
     string private name;
-    
+
     constructor(string memory _name) {
         name = _name;
     }
+    
     function changeName(string memory _name) public {
         name = _name;
     }
+
     function getName() public view returns (string memory) {
         return name;
     }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-waffle";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import '@oasisprotocol/sapphire-hardhat';
+
 import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    sapphiretest: {
+      // This is Testnet! If you want Mainnet, add a new network config item.
+      url: "https://testnet.sapphire.oasis.dev",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+      chainId: 0x5aff,
+    },
+  },
 };
 
 export default config;
